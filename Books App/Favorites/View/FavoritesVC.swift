3b1d9b99c448cadc317fab46abbf610086c18c5b
Berkay <berkayyalcn1@gmail.com>
2022-10-13 00:06:07 +0300

    Created collectionView

--- Books App/Favorites/View/FavoritesVC.swift
+++ Books App/Favorites/View/FavoritesVC.swift
@@ -9,9 +9,51 @@ import UIKit
 
 class FavoritesVC: UIViewController {
 
+    @IBOutlet weak var favoritesCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Favoriler"
+    }
+    
+    func setupUI() {
+        favoritesCollectionView.delegate = self
+        favoritesCollectionView.dataSource = self
+        registerCollectionView()
+    }
+    
+    func registerCollectionView() {
+        
+    }
+}
+
+extension FavoritesVC: UICollectionViewDelegate {
+    
+}
+
+extension FavoritesVC: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension FavoritesVC: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: (collectionView.frame.size.width - 30) / 2, height: 235)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
     }
 }
