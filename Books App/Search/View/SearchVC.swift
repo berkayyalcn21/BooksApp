1113b5eb1f2bb4a3aabe195b37da9be7ded0ed5f
Berkay <berkayyalcn1@gmail.com>
2022-10-14 15:15:46 +0300

    Added picker and table view

--- Books App/Search/View/SearchVC.swift
+++ Books App/Search/View/SearchVC.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
+    var pickerView = UIPickerView()
+    var list = ["Test","Test","Test","Test","Test","Test"]
+    @IBOutlet weak var searchTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "Arama"
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        categoryTextField.inputView = pickerView
+    }
+}
+
+
+extension SearchVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoryTextField.text = list[row]
     }
 }
